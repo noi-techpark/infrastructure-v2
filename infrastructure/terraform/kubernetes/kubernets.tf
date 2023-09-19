87006338f3b7e4c94f719163448418e0b9ccfbd5
Simon Dalvai <s.dalvai@noi.bz.it>
2023-09-19 15:21:47 +0200

    readd compute addons

--- infrastructure/terraform/kubernetes/kubernets.tf
+++ infrastructure/terraform/kubernetes/kubernets.tf
@@ -76,28 +76,28 @@ locals {
 }
 
 # Addons.
-locals {
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-      before_compute = true
-      configuration_values = jsonencode({
-        env = {
-          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
-          ENABLE_PREFIX_DELEGATION = "true"
-          WARM_PREFIX_TARGET       = "1"
-        }
-      })
-    }
-    aws-ebs-csi-driver = {
-      most_recent              = true
-      service_account_role_arn = "arn:aws:iam::${local.account_id}:role/${data.aws_eks_cluster.default.id}-ebs-csi-controller"
-    }
-  }
-}
+ locals {
+   cluster_addons = {
+     coredns = {
+       most_recent = true
+     }
+     kube-proxy = {
+       most_recent = true
+     }
+     vpc-cni = {
+       most_recent = true
+       before_compute = true
+       configuration_values = jsonencode({
+         env = {
+           # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
+           ENABLE_PREFIX_DELEGATION = "true"
+           WARM_PREFIX_TARGET       = "1"
+         }
+       })
+     }
+     aws-ebs-csi-driver = {
+       most_recent              = true
+       service_account_role_arn = "arn:aws:iam::${local.account_id}:role/${data.aws_eks_cluster.default.id}-ebs-csi-controller"
+     }
+   }
+ }
