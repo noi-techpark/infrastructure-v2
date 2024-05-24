09a40911fd0a098fae5eacb6f6158444813a29dd
Clemens Zagler <c.zagler@noi.bz.it>
2024-05-24 09:41:45 +0200

    detour: curl instead of wget

--- devops/detour/run.sh
+++ devops/detour/run.sh
@@ -7,7 +7,7 @@
 # kubectl port-forward -n core svc/rabbitmq-headless 15672 --address 0.0.0.0 &
 
 # download tool directly from endpoint so that we have matching version
-wget -rq http://localhost:15672/cli/rabbitmqadmin
+curl -O -q http://localhost:15672/cli/rabbitmqadmin
 chmod +x rabbitmqadmin
 RABBIT_PW=`kubectl get secret -n collector rabbitmq-svcbind -o  jsonpath='{.data.password}' | base64 -d`
 radmin () {
@@ -17,11 +17,11 @@ radmin () {
 srcroute=echarging.route220
 detourroute=$srcroute.detour
 
-radmin declare queue name=$srcroute.detour
-radmin declare binding source=routed destination=$detourroute routing_key=$srcroute
-shoveljson='{"src-uri":"amqp://","src-queue":"'$srcroute'","dest-uri":"amqp://","dest-queue":"'$detourroute'","src-delete-after":"queue-length"}'
-radmin declare parameter component=shovel name=testshovel value=$shoveljson
+# radmin declare queue name=$srcroute.detour
+# radmin declare binding source=routed destination=$detourroute routing_key=$srcroute
+# shoveljson='{"src-uri":"amqp://","src-queue":"'$srcroute'","dest-uri":"amqp://","dest-queue":"'$detourroute'","src-delete-after":"queue-length"}'
+# radmin declare parameter component=shovel name=testshovel value=$shoveljson
 
-radmin delete parameter component=shovel name=testshovel
-radmin delete queue name=test.detour
-radmin delete queue name=test.detour.target
+# radmin delete parameter component=shovel name=testshovel
+# radmin delete queue name=test.detour
+# radmin delete queue name=test.detour.target
