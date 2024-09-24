76564c19da12fc15f9a7d0201cf254e8709ea85c
Clemens Zagler <c.zagler@noi.bz.it>
2024-09-24 15:08:17 +0200

    terraform kubernetes update users

--- infrastructure/terraform/kubernetes/env.tf
+++ infrastructure/terraform/kubernetes/env.tf
@@ -5,18 +5,17 @@ locals {
   eks_main_pool_instance_types = local.prod ? ["t3a.large"] : ["r7a.large"]
 
   eks_admins_dev = [
-    "animeshon",
     "c.zagler",
     "m.rabanser",
     "r.thoeni",
-    "s.dalvai",
     "s.seppi",
+    "l.pasquarelli",
   ]
   eks_admins_prod = [
     "c.zagler",
     "r.thoeni",
-    "s.dalvai",
     "s.seppi",
+    "l.pasquarelli",
   ]
   eks_admins = local.prod ? local.eks_admins_prod : local.eks_admins_dev
 }
