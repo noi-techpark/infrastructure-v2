222ab57d0a0a8f12d5b8689d13aeae3320d2895b
Clemens Zagler <c.zagler@noi.bz.it>
2024-11-05 11:35:33 +0100

    devops/encore: 	- configure via .env file 	- use unique ports for forwarding 	- trap forwarding cleanup

--- devops/encore/run.sh
+++ devops/encore/run.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
 # Port-forward services
-kubectl port-forward -n core svc/rabbitmq-headless 5672 --address 0.0.0.0 &
+kubectl port-forward -n core svc/rabbitmq-headless 3001:5672 --address 0.0.0.0 &
 forwards=$!
-kubectl port-forward -n core svc/mongodb-headless 27017 --address 0.0.0.0 &
+kubectl port-forward -n core svc/mongodb-headless 3002:27017 --address 0.0.0.0 &
 forwards="$forwards $!"
+
+cleanup()
+{
+for pid in $forwards; do
+    kill $pid
+done
+}
+
+trap cleanup EXIT 
 
 # Connection details for rabbit and mongo. Run kubectl port-forward above beforehand
 RABBIT_PW=`kubectl get secret -n collector rabbitmq-svcbind -o  jsonpath='{.data.password}' | base64 -d`
 MONGO_PW=`kubectl get secret -n collector mongodb-collector-svcbind -o  jsonpath='{.data.password}' | base64 -d`
-export RABBIT_URI="amqp://opendatahub:${RABBIT_PW}@localhost:5672"
-export MONGO_URI="mongodb://collector:${MONGO_PW}@localhost/?tls=false&ssl=false&directConnection=true"
+export RABBIT_URI="amqp://opendatahub:${RABBIT_PW}@localhost:3001"
+export MONGO_URI="mongodb://collector:${MONGO_PW}@localhost:3002/?tls=false&ssl=false&directConnection=true"
 
-# Mongodb db and collection that is queried
-export DB="municipality-bolzano"
-export COLLECTION="parking-macello"
-
-# Queue the messages are pushed into
-export QUEUE="municipality-bolzano.parking-macello.temp"
-
-# A mongodb query that is applied to the collection
-# Following the "relaxed" format of mongodb extended json: https://www.mongodb.com/docs/manual/reference/mongodb-extended-json/
-# e.g. Dates have to be full ISO not just partial
-export QUERY='{ "bsontimestamp": { "$gte": { "$date": "2024-06-21T00:00:00.000+00:00"}}}'
+if [ ! -f .env ]; then
+    echo ".env file does not exist. Copy and customize the example file"
+    exit 1
+fi
+source .env
 
 go mod download
 go run main.go
-
-for pid in $forwards; do
-    kill $pid
-done
