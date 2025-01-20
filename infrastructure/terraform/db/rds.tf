dd98a782a9e0e0272258d94eb7222a0341ac57ff
Clemens Zagler <c.zagler@noi.bz.it>
2025-01-20 11:21:36 +0100

    activate deletion protection, as testing DB is not live

--- infrastructure/terraform/db/rds.tf
+++ infrastructure/terraform/db/rds.tf
@@ -34,6 +34,6 @@ resource "aws_db_instance" "pg-timeseries" {
   maintenance_window = "Thu:01:15-Thu:01:45"
   auto_minor_version_upgrade = true
 
-  deletion_protection = false
+  deletion_protection = true
   apply_immediately = false
 }
