3543027d78274dc3073407f10ab1b95b48834db1
Clemens Zagler <c.zagler@noi.bz.it>
2025-08-12 10:42:33 +0200

    tf: scale up production cluster, remove lpasquarelli

--- infrastructure/terraform/kubernetes/env.tf
+++ infrastructure/terraform/kubernetes/env.tf
@@ -2,21 +2,19 @@
 locals {
   prod = var.ENVIRONMENT == "prod"
 
-  eks_main_pool_instance_types = local.prod ? ["t3a.large"] : ["r7a.large"]
+  eks_main_pool_instance_types = local.prod ? ["r6a.large"] : ["r7a.large"]
 
   eks_admins_dev = [
     "c.zagler",
     "m.rabanser",
     "r.thoeni",
     "s.seppi",
-    "l.pasquarelli",
     "m.roggia",
   ]
   eks_admins_prod = [
     "c.zagler",
     "r.thoeni",
     "s.seppi",
-    "l.pasquarelli",
     "m.roggia",
   ]
   eks_admins = local.prod ? local.eks_admins_prod : local.eks_admins_dev
