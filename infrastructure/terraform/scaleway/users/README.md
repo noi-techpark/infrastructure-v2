aca42b1c56197f12f5b682a7fb47ca86591a67be
Clemens Zagler <c.zagler@noi.bz.it>
2026-05-04 17:27:11 +0200

    tf: working user creation on scaleway

---
+++ infrastructure/terraform/scaleway/users/README.md
@@ -0,0 +1,6 @@
+Needs first created via console:
+
+- Application type IAM user
+- API Key set to correct default projects
+- Create a bucket in that same default project
+- Add a bucket policy granting full access to resources <bucketname> and <bucketname>/* (the second one is not there by default) to the application user
--- infrastructure/terraform/scaleway/users/backend.tf
+++ infrastructure/terraform/scaleway/users/backend.tf
@@ -3,7 +3,17 @@
 ################################################################################
 
 terraform {
-  backend "local" {}
+  backend "s3" {
+    key                         = "users/terraform.tfstate"
+    region                      = "fr-par"
+    endpoints = {
+      s3 = "https://s3.fr-par.scw.cloud"
+    }
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    bucket                      = "tfstate-42"
+  }
 
   required_providers {
     scaleway = {
