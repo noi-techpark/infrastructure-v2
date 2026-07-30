4546ef5db5c4acac078e57396e2a1607c2132f82
Clemens Zagler <c.zagler@noi.bz.it>
2026-07-30 14:59:30 +0200

    move terraform resources to private repository

--- infrastructure/terraform/dns/ip.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_eip" "eks-ingress-b" {    
-    # This is our main IP address, probably should make sure it's not given up
-    lifecycle {
-      prevent_destroy = true
-    }
-}
-resource "aws_eip" "eks-ingress-private" {    
-}
--- infrastructure/terraform/scaleway/it/project.tf
+++
@@ -1,9 +0,0 @@
-################################################################################
-## This file creates the Scaleway project for the IT department.
-## https://registry.terraform.io/providers/scaleway/scaleway/latest/docs/resources/account_project
-################################################################################
-
-resource "scaleway_account_project" "it" {
-  name        = "IT"
-  description = "Project for the IT department"
-}
